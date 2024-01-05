b9ff751eaee469b088da93d4df0064aba6da9999
gbarrett28 <geoff@barwood.org.uk>
2024-01-05 16:04:17 +0000

    Fixed couple of bugs.

--- num_rec.py
+++ num_rec.py
@@ -1,5 +1,9 @@
 import re
 
+import cv2
+import numpy
+from matplotlib import pyplot
+
 from sklearn.preprocessing import Normalizer
 from inp_image import *
 from inp_image import number_img
@@ -11,11 +15,15 @@ class NumberRecogniser:
 		self.pca = PCA()
 		self.kmeans = KMeans(n_clusters=16, n_init=16)
 
-		allmoms = find_moments(allcs)
+		allmoms = []
+		for ml in find_moments(allcs):
+			momsv = np.zeros(35, dtype=np.float64)
+			momsv[:len(ml)] = np.array(ml)
+			allmoms.append(momsv)
 		num_var = self.pca.fit_transform(self.norm.fit_transform(allmoms))
 		labels = self.kmeans.fit_predict(num_var)
 
-		show_clusters(allcs)
+		show_clusters(labels, allcs)
 		self.show_scatter(labels, num_var)
 
 	def show_scatter(self, labels, num_var):
@@ -62,3 +70,22 @@ def find_moments(nums):
 			moms += rmoms
 		ret.append(moms)
 	return ret
+
+
+def plot_pca(pca, dim=32):
+	print(pca.explained_variance_ratio_[0:32])
+	print(np.cumsum(pca.explained_variance_ratio_[0:32]))
+	# print(pca.components_[0].shape)
+
+	for c in range(dim):
+		pmin = pca.components_[c].min()
+		pmax = pca.components_[c].max()
+		# gry_weights = np.reshape(np.uint8((pca.components_[c] - pmin) * 255 / (pmax - pmin)), (yn, xn))
+		gry_weights = np.uint8((pca.components_[c] - pmin) * 255 / (pmax - pmin))
+		col_weights = cv2.applyColorMap(gry_weights, cv2.COLORMAP_JET)
+
+		plt.subplot(4, 8, c + 1)
+		plt.imshow(col_weights)
+		plt.xticks([]), plt.yticks([])
+
+	plt.show()
